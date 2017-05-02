cd1eb15400231ece397771ffa1c63f13b2353288
libaitaohong <492563822@qq.com>
2017-05-02 20:35:03 +0800

    访问数据库

---
+++ src/main/java/com/mybatis/db/DBAccess.java
@@ -0,0 +1,27 @@
+package com.mybatis.db;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+/**
+ * Created by zhanghe on 2017/4/23.
+ * 访问数据库类
+ */
+public class DBAccess {
+     public SqlSession getSqlSession() throws IOException {
+        //通过配置文件获取数据库连接
+         //通过Resources类获取Reader这个对象，然后给出核心配置文件所在路径
+         //将异常抛给dao层去处理
+         Reader reader = Resources.getResourceAsReader("Configuration.xml");
+         //通过配置信息构建一个SqlSessionFactory
+         SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(reader);
+         //通过sqlSessionFactory打开一个数据库会话
+         SqlSession sqlSession = sqlSessionFactory.openSession();
+         return sqlSession;
+     }
+}
